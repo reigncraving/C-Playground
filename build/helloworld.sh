8b131ec3feb635357225d28ec6c5a32126e78f5b
reigncraving <reigncraving@yahoo.com>
2023-03-17 01:52:23 +0200

    Extracted colors to separate file

--- build/helloworld.sh
+++ build/helloworld.sh
@@ -1,10 +1,6 @@
 #!/bin/bash  
 
-BLUE='\033[0;34m'
-WHITE='\033[0;37m' 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-PURPLE='\033[0;35m'
+source ./build/colors.sh
 
 echo -e "${BLUE}[Compiling...]" 
 echo -e "${GREEN}> ${PURPLE}Helloworld program."
