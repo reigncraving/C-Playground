6c2b5d6a139983bdc22e53b8896d6fecd7bdadc5
reigncraving <reigncraving@yahoo.com>
2023-03-27 02:59:32 +0300

    Completed first chanlenge

--- challenges/ch_1/main.c
+++ challenges/ch_1/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+/* Global variable accessed only in this file: */
+static double gd = 3.42;
+float uniFloat = 1.32;
+
+static int func (void) {
+    printf("Static function defined");
+    return 0;
+}
+
 int main() {
-    printf("Hello");
+    /* int with block scope and temporary storage */
+    int temp = 0;
+    register int newTemp = temp + 1;
+    static float lastTemp = 3.2;
+
+    printf("Integer with temp scope %i, %p\n", temp, &temp);
+    printf("Global double with file scope %.3f\n", gd);
 
     return 0;
 }
